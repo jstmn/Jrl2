6219189024f7e61bce7255f0f06cecf08f1e0e6b
Jeremy Morgan <jsmorgan6@gmail.com>
2025-09-01 19:50:31 -0700

    first commit

--- tests/test_scene.py
+++ tests/test_scene.py
@@ -1,8 +1,8 @@
-from jrl2.scene import SingleEnvironmentScene
-
-from jrl2.robots import Panda
-
+import torch
 import pytest
+
+from jrl2.scene import SingleEnvironmentScene
+from jrl2.robots import Panda
 
 
 @pytest.fixture
@@ -10,8 +10,15 @@ def single_environment_scene():
     return SingleEnvironmentScene(Panda())
 
 
-def test_single_environment_scene(single_environment_scene):
+def test_single_environment_scene(single_environment_scene: SingleEnvironmentScene):
     assert single_environment_scene is not None
+
+    q = torch.tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    single_environment_scene.add_sphere(torch.tensor([2.0, 0.0, 0.0]), 1.0)
+    single_environment_scene.add_sphere(torch.tensor([2.2, 0.0, 0.0]), 0.1)
+    collisions = single_environment_scene.find_robot_environment_collisions(q)
+    assert len(collisions) == 0
+
 
 
 # @pytest.fixture
